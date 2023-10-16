1c09a5c70cf946927243a08095e690a76365e759
IsaacBurbidge <114247599+IsaacBurbidge@users.noreply.github.com>
2023-10-16 23:43:22 +0100

    Update

--- Conversion_Script.py
+++ Conversion_Script.py
@@ -5,50 +5,41 @@ import numpy as np
 
 data = pd.read_csv("cities.csv")
 
-#emptyArray = []
-
-#tableofleastdistances = pd.DataFrame(emptyArray)
-
-#for i in data.index:
-#    tableofleastdistances = tableofleastdistances.assign(**{str(i): []})
-#print(tableofleastdistances)
-
-tableofleastdistances = pd.read_csv("coverteddata.csv")
+tableofleastdistances = []
 
 def _create_distance_matrix():
-    i = 0
-    nullList = []
-    for item in data.index:
-        i += 1
-        nullList.append(-1)
+
    
-    i = 0
-    for item in data.index:
-        tableofleastdistances.loc[len(tableofleastdistances)] = nullList
-        i += 1
+    for i in range(0,5000):
+        nullList = []
+        for j in range(0,5000):
+            nullList.append(-1)
+        tableofleastdistances.append(nullList)
     print("matrixmade")
 
 def _find_initial_distances():
-    for i in data.index:
-        for j in data.index:
+    for i in range(0,5000):
+        for j in range(0,5000):
             if i == j:
-                break
+                pass
             elif tableofleastdistances[i][j] == -1:
                 x_length = data.at[data.index[i],'X'] + data.at[data.index[j],'X']
                 y_length = data.at[data.index[i],'Y'] + data.at[data.index[j],'Y']
                 path_length = (x_length**2) + (y_length**2)
                 path_length = math.sqrt(path_length)
-                tableofleastdistances.at[i, j] = path_length
-                tableofleastdistances.at[j, i] = path_length
+                tableofleastdistances[i][j] = path_length
+                tableofleastdistances[j][i] = path_length
     print("distancesinitialised")
 
+
 def _write_to_file():
-    #newdata = pd.DataFrame(tableofleastdistances)
-    tableofleastdistances.to_csv('coverteddata.csv', index=False)
+    dataToWrite = pd.DataFrame(tableofleastdistances)
+    #print(dataToWrite)
+    dataToWrite.to_csv('coverteddata.csv', index = True)
 
 def main():
     _create_distance_matrix()
-    #_find_initial_distances()
+    _find_initial_distances()
     _write_to_file()
 
 
