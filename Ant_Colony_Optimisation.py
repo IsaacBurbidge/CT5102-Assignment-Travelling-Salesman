ede1fbbed6738d5299a1f64ef2608a7ce5d5da53
IsaacBurbidge <114247599+IsaacBurbidge@users.noreply.github.com>
2023-11-15 13:22:17 +0000

    Update

--- Ant_Colony_Optimisation.py
+++ Ant_Colony_Optimisation.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import Pheremone_Script as ps
+import Pheromone_Script as ps
 import math
 import random
 import numpy
@@ -7,30 +7,31 @@ import numpy
 EPSILON=1e-5
 
 def _find_shortest_route():
-    global pheremones, data
+    global pheromones, data
     antsperiteration = int(input("How many Ants per iteration?"))
     iterations = int(input("How many iterations?"))
     data = pd.read_csv("converteddata.csv")
-    ps._generate_pheremones(data.shape[0])
-    pheremones = pd.read_csv("pheremones.csv")
+    #ps._generate_pheromones(data.shape[0])
+    pheromones = pd.read_csv("pheromones.csv")
     _run_ants(antsperiteration, iterations)
 
-def _degrade_pheremones(pheremone):
-    if pheremone == -1:
+def _degrade_pheromones(pheromone):
+    if pheromone == -1:
         return -1
     else:
-        return int((pheremone *0.95)//1)
+        return int((pheromone *0.95)//1)
 
-def _add_pheremones(route):
+def _add_pheromones(route):
     for i in range (0, data.shape[0]):
         nextindex = (i+1)%data.shape[0]
-        pheremones.iat[route[i],route[nextindex]] = int((pheremones.iat[route[i],route[nextindex]]*1.2)//1)
+        pheromones.iat[route[i],route[nextindex]] = int((pheromones.iat[route[i],route[nextindex]]*1.2)//1)
 
 def _traverse_graph(startnode):
     ALPHA = 0.9
     BETA = 1.5
     
-    visitedList = list(range(0,data.shape[0]))
+    
+    visitedList = numpy.asarray([1 for _ in range(data.shape[0])])
     visitedList[startnode] = 0
     
     route = []
@@ -44,7 +45,7 @@ def _traverse_graph(startnode):
         
         for i in range(0,data.shape[0]):
             if visitedList[i] != 0:
-                pheromone_level = max(pheremones.iat[currentnode, i], EPSILON) #constant added to encourage exploration
+                pheromone_level = max(pheromones.iat[currentnode, i], EPSILON) #constant added to encourage exploration
                 v = math.pow(pheromone_level,ALPHA) / math.pow(data.iat[currentnode, i], BETA)
                 jumps_neighbors.append(i)
                 jumps_values.append(v)
@@ -62,15 +63,15 @@ def _traverse_graph(startnode):
 
 
 def _run_ants(antsperiteration, iterations):
-    global pheremones
+    global pheromones
     
     bestroute = []
     bestdistance = 0
     for i in range(0, iterations):
-        antRouteList = [_traverse_graph(random.randint(0,data.shape[0])) for i in range (0, antsperiteration)]
+        antRouteList = [_traverse_graph(random.randint(0,data.shape[0]-1)) for i in range (0, antsperiteration)]
         antRouteList.sort(key = lambda x: x[1])
         for j in range(0,len(antRouteList)):
-            _add_pheremones(antRouteList[j][0])
+            _add_pheromones(antRouteList[j][0])
         if bestroute == []:
             bestroute = antRouteList[0][0]
             bestdistance = antRouteList[0][1]
@@ -80,26 +81,49 @@ def _run_ants(antsperiteration, iterations):
         print(i)
         print(antRouteList[0])
         print(bestdistance)
-        pheremones = pheremones.map(_degrade_pheremones)
-        print(pheremones)
+        pheromones = pheromones.map(_degrade_pheromones)
+        print(pheromones)
     print(bestroute)
     print(bestdistance)
     _write_best_distance_to_file(bestroute, bestdistance)
-    #_write_pheremones_to_file()
+    #_write_pheromones_to_file()
   
-def _write_pheremones_to_file():
-    pheremones.to_csv('finalpheremones.csv', index = False)
+def _write_pheromones_to_file():
+    pheromones.to_csv('finalpheromones.csv', index = False)
 
 def _write_best_distance_to_file(bestroute, bestdistance):
-    #with open('Distance.txt') as file:
+    previousdist = math.inf
+    with open('BestDistance.txt') as file:
+        line = file.readline()
+        i = 0
+        while line:
+            if i == 0:
+                characterlist = []
+                length = ""
+                number = False
+                for j in range(0, len(line)):
+                    if line[j].isnumeric() or line[j] == ".":
+                        characterlist.append(line[j])
+                        pass
+                    else:
+                        pass
+                length = "".join(characterlist)
+                previousdist = float(length)
+                print(" a = " + str(previousdist))
+                i += 1
+            else:
+                previousroute = []
+                pass
+            line = file.readline()
+        pass
+
+    if bestdistance < previousdist:
+        linesToWrite = ["Best Distance: "+str(bestdistance), "Best Route: "+str(bestroute)]
+        with open('BestDistance.txt', 'w') as file:
+            for line in linesToWrite:
+                file.write(line)
+                file.write("\n")
         
-    
-    linesToWrite = ["Best Distance = "+str(bestdistance), "Best Route = "+str(bestroute)]
-    with open('Distance.txt', 'w') as file:
-        for line in linesToWrite:
-            file.write(line)
-            file.write("\n")
-    
 
 
 
---
+++ Pheromone_Script.py
@@ -0,0 +1,25 @@
+import pandas as pd
+
+pheromonegraph = []
+
+#Initialises a 5000x5000 matrix
+def _create_pheromone_matrix(pheromonegraphlength):
+    for i in range(0,pheromonegraphlength):
+        pheromoneList = []
+        for j in range(0,pheromonegraphlength):
+            if i != j:
+                pheromoneList.append(100) # a base level of pheromones, equal across all paths 
+            else:
+                pheromoneList.append(-1) # null, aka. cant visit. 
+        pheromonegraph.append(pheromoneList)
+
+#Saves the pheromone graph as an external file
+def _write_to_file():
+    dataToWrite = pd.DataFrame(pheromonegraph)
+    dataToWrite.to_csv('pheromones.csv', index = False)
+
+#External function called to run this script
+def _generate_pheromones(pheromonegraphlength):
+    _create_pheromone_matrix(pheromonegraphlength)
+    _write_to_file()
+
